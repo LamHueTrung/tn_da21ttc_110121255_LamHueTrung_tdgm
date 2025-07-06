ef40547f4b89f9173f88bbc567abef60317a3433
LHT <93436510+LamHueTrung@users.noreply.github.com>
2025-07-06 15:25:28 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const setupSwagger = require('../src/app/Extesions/swagger');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
-const port = Math.floor(1024 + Math.random() * 40000);
+const port = 4000;
 
 // Kết nối tới MongoDB
 connectDB();
